7f820f1332c65c8a056a57e2ab6c48118a81424a
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-15 07:51:05 +0100

    server code

--- src/server.js
+++ src/server.js
@@ -1,2 +1,22 @@
-"use strict";
-console.log("server.js file was loaded");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { after } = require("node:test");
+
+const app = express();
+
+const { port } = require("./config");
+
+// Meddleware
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ msg: "home route" });
+});
+
+// app Listen
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
